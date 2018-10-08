35be7615cc5fa4afaac582d4b2f99b1f232e848b
Ryan Shaver <32579646+ACloseShave@users.noreply.github.com>
2018-10-07 21:11:40 -0500

    Update shaver_ryan_lab_3_part_A.py
    Updated the output by limiting to 2 decimal places
    Condensed the conversion of the input to single-line for clarity
    Planned integration of loops in the next release

--- shaver_ryan_lab_3_part_A.py
+++ shaver_ryan_lab_3_part_A.py
@@ -1,48 +1,43 @@
 #Preparing Variables and data types
 
 #km_convert = miles * 1.6
-miles = input("Hey Cousin Mark! How many miles do you want converted to kilometers? ")
-miles = int(miles)
+miles = float(input("Hey Cousin Mark! How many miles do you want converted to kilometers? "))
 if miles < 0:
     print("Error: Mark, don't tell me negative numbers!")
 else:
     kilometers = miles * 1.6
-    print("Okay, Mark, that is", kilometers, "kilometers!")
+    print("Okay, Mark, that is", format(kilometers,'.2f'), "kilometers!")
 
     #celsius_convert = (farenheit - 32) * (5/9)
-    faren = input("Now, Markie parkie, how many degrees Farenheit should we convert to Celsius? ")
-    faren = int(faren)
+    faren = float(input("Now, Markie parkie, how many degrees Farenheit should we convert to Celsius? "))
     if faren > 1000:
         print("Error: Hey, you know that's too hot, Mark!")
     elif faren < 0:
         print("Error: Seriously? Mark, I said positive numbers. Be reasonable!")
     else:
         celsius = (faren - 32) * (5/9)
-        print("Great, Mark! So, that is", celsius, "degrees Celsius!")
+        print("Great, Mark! So, that is", format(celsius,'.2f'), "degrees Celsius!")
 
         #liter_convert = gallons * 3.9
-        gallons = input("Now, Markimus, how many gallons shall we convert to liters? ")
-        gallons = float(gallons)
+        gallons = float(input("Now, Markimus, how many gallons shall we convert to liters? "))
         if gallons < 0:
             print("Error: Dude, Mark, stop giving me negative numbers!")
         else:
             liters = gallons * 3.9
-            print("Great, so that would be", liters, "liters!")
+            print("Great, so that would be", format(liters,'.2f'), "liters!")
 
             #kg_convert = pounds * .45
-            pounds = input("Markipoo, tell me how many pounds to convert to kilograms: ")
-            pounds = float(pounds)
+            pounds = float(input("Markipoo, tell me how many pounds to convert to kilograms: "))
             if pounds < 0:
                 print("Error: You're pushing your luck with these negatives, Markus!")
             else:
                 kilograms = pounds * .45
-                print("That is", kilograms, "in kilograms, cousin!")
+                print("That is", format(kilograms,'.2f'), "in kilograms, cousin!")
 
                 #cm_convert = inch * 2.54
-                inches = input("Finally, how many inches do you want converted to centimeters? ")
-                inches = float(inches)
+                inches = float(input("Finally, how many inches do you want converted to centimeters? "))
                 if inches < 0:
                     print("Error: ...is this some kind of game to you? POSITIVE numbers, Mark!")
                 else:
                     centi = inches * 2.54
-                    print("Okay, running the numbers...it's", centi, "centimeters!")
+                    print("Okay, running the numbers...it's", format(centi,'.2f'), "centimeters!")
