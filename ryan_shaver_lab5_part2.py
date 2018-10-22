144d4661d72923c9fc558f00484cd3f0ccfac63f
Ryan Shaver <32579646+ACloseShave@users.noreply.github.com>
2018-10-21 23:59:47 -0500

    Removed older commented out bits
    Removed commented functions that had previously been externalized

--- ryan_shaver_lab5_part2.py
+++ ryan_shaver_lab5_part2.py
@@ -2,16 +2,12 @@
 ##  Program to convert measurements  ##
 #######################################
 
-
 #import statement
 import conversion_functions
-
-
 
 #greeting and explanation of program
 print("Hey cousin Mark!")
 print("Let's convert some measurements together.")
-
 
 def main():
     #reset while loop interruptor
@@ -75,35 +71,5 @@ def main():
             InchesToCm(inches)
             limit = 3
     limit = 0
-
-
-###################################################
-##  Conversion functions called by Lab 5 Part 2  ##
-###################################################
-
-##def MilesToKm(milesToKm):
-##    kilometers = milesToKm * 1.6
-##    print("Okay, Mark, that is", format(kilometers, '.2f'), "kilometers!")
-##    
-##
-##def FahToCel(FahToCel):
-##    celsius = (FahToCel - 32) * (5/9)
-##    print("Great! So, that is", format(celsius, '.2f'), "degrees Celsius!")
-##    
-##
-##def GalToLit(GalToLit):
-##    liters = GalToLit * 3.9
-##    print("Great, so that would be", format(liters,'.2f'), "liters!")
-##    
-##
-##def PoundsToKg(PoundsToKg):
-##    kilograms = PoundsToKg * .45
-##    print("That is", format(kilograms,'.2f'), "kilograms, cousin!")
-##    
-##
-##def InchesToCm(InchesToCm):
-##    centi = InchesToCm * 2.54
-##    print("Okay, running the numbers...it's", format(centi, '.2f'), "centimeters!")
-
         
 main()
